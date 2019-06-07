ac8f983030bfb3ec5ec27b45eb8c1344fff59e57
Malory Rose <rosem15@uw.edu>
2019-06-06 20:54:40 -0700

    good to go

--- TEAM_TV_SHOWS/BusinessRules.sql
+++ TEAM_TV_SHOWS/BusinessRules.sql
@@ -1,6 +1,8 @@
 USE Group13_TV_SHOWS
 GO 
+
 /*Malory's Code*/
+
 -- business rule 1
 --Can only watch Netflix if country is not the US
 CREATE FUNCTION fn_OnlyNetflixOutsideOfUS()
@@ -24,6 +26,7 @@ ALTER TABLE tblPLATFORM
 ADD CONSTRAINT CK_OnlyNetflixOutsideOfUS
 CHECK (dbo.fn_OnlyNetflixOutsideOfUS() = 0) 
 GO 
+
 -- business rule 2
 -- If you are from Florida you cannot watch a horror movie unless you are  18 or older
 CREATE FUNCTION fn_NonHorroMoviesUnder18inFLorida()
@@ -52,6 +55,7 @@ CHECK (dbo.fn_NonHorroMoviesUnder18inFLorida() = 0)
 GO 
 
 /*Madisen's code*/
+
 --business rule: users from Spain can only use Hulu or Netflix
 CREATE FUNCTION fn_OnlyHuluOrNetflixInSpain()
 RETURNS INT 
@@ -76,6 +80,7 @@ ALTER TABLE tblPLATFORM
 ADD CONSTRAINT CK_OnlyHuluOrNetflixInSpain
 CHECK (dbo.fn_OnlyHuluOrNetflixInSpain() = 0) 
 GO 
+
 -- business rule: users from Germany can only use platforms if they became a member before 2015
 CREATE FUNCTION fn_Germany2015Membership()
 RETURNS INT 
@@ -98,7 +103,9 @@ ALTER TABLE tblPLATFORM
 ADD CONSTRAINT CK_Germany2015Membership
 CHECK (dbo.fn_Germany2015Membership() = 0) 
 GO 
+
 /*Ria's code*/
+
 -- business rule ria
 --Can not watch horror movie 
 --if younger than 16
@@ -149,6 +156,7 @@ CHECK (dbo.fn_ageappropriatmembership() = 0)
 GO 
 
 /*Xiefi's Code*/
+
 --If you were between the age 18 and 25 then you are not permitted to download a movie between 0 to 4am
 CREATE function No_15_25_downlaod_movie_past_12()
 returns int
@@ -169,6 +177,7 @@ add
 constraint No_15_25_downlaod_movie_past_mid
 check (dbo.No_15_25_downlaod_movie_past_12() = 0)
 Go
+
 -- If you are from washington state and have a membership record less than a year, you cannot answer survey question
 Create function No_WA_Survey_Membership_less_than_1()
 returns int 
